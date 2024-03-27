8c5f86ee008980d8d6f265aed8765c2020a58361
Ro1984Bi <bilbaorodrigojavi@outlook.com>
2024-03-27 14:39:39 -0400

    create welcome page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,36 @@
-
+import { Button } from "@/components/ui/button";
+import { RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
 
 export default function Home() {
   return (
-  <div></div>
+    <section className="flex items-center justify-center bg-background h-[90vh]">
+      <div className="relative items-center w-full px-5 py-12 mx-auto lg:px-16 max-w-7xl md:px-12">
+        <div className="max-w-3xl mx-auto text-center">
+          <div>
+            <span className="w-auto px-6 py-3 rounded-full bg-secondary">
+              <span className="text-sm font-medium text-primary">
+                Sort your notes easily
+              </span>
+            </span>
+            <h1 className="mt-8 text-3xl font-extrabold tracking-tight lg:text-6xl">
+              Create Notes with ease
+            </h1>
+            <p className="max-w-xl mx-auto mt-8 text-base lg:text-xl text-secondary-foreground">
+              Software as a Service (SaaS) is a cloud-based software model that
+              delivers applications to end users through an Internet browser.
+              SaaS providers host services and applications so that customers
+              can access them on demand.
+            </p>
+          </div>
+          <div className="flex justify-center max-w-sm mx-auto mt-10">
+            <RegisterLink>
+            <Button size={"lg"} className="rounded-full">
+              Sign Up for free
+            </Button>
+            </RegisterLink>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
